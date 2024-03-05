410472ee34e11be97785c5be132a28369600a0e7
Semyon Adaikin <brave.rabbit@gmail.com>
2024-03-05 19:37:22 +0300

    error

--- Check200.sh
+++ Check200.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 response=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:9889)
-if [ "$response" == "200" ]; then
+if [ "$response" == "201" ]; then
 echo "Код ответа: $response"
 #"Тест успешно пройден!"
 curl -s -X POST https://api.telegram.org/bot6496646075:AAG77ORDHogHY7S0jZ83hxnnG31kO-ZbXhU/sendMessage -d chat_id=253758547  -d text="Тест успешно пройден!"
