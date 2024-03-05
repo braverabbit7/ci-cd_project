8a71538ace638e272c2ae7fd976b356657c6361f
Semyon Adaikin <brave.rabbit@gmail.com>
2024-03-05 19:33:51 +0300

    alarm added

--- Check200.sh
+++ Check200.sh
@@ -2,10 +2,11 @@
 response=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:9889)
 if [ "$response" == "200" ]; then
 echo "Код ответа: $response"
-echo "Тест успешно пройден!"
+#"Тест успешно пройден!"
+curl -s -X POST https://api.telegram.org/bot6496646075:AAG77ORDHogHY7S0jZ83hxnnG31kO-ZbXhU/sendMessage -d chat_id=253758547  -d text="Тест успешно пройден!"
 exit 0
 else
 echo "Код ответа: $response"
-echo "Тест не пройден!"
+curl -s -X POST https://api.telegram.org/bot6496646075:AAG77ORDHogHY7S0jZ83hxnnG31kO-ZbXhU/sendMessage -d chat_id=253758547  -d text="Тест не пройден!"
 exit 1
 fi
